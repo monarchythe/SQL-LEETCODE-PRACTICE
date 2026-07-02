43837835ae8139d2a846dc9dc1d2bec726760d41
Monarch Nigam <133744291+monarchythe@users.noreply.github.com>
2026-07-02 07:27:01 -0500

    Delete 176-Second-Highest-Salary.sql

--- 176-Second-Highest-Salary.sql
+++
@@ -1,8 +0,0 @@
--- Write your PostgreSQL query statement below
-select max(salary) as SecondHighestSalary 
-from (
-    SELECT DISTINCT salary 
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
-    );
